942b652b590fdd2a1526293ab6ac8dba3907fd1a
Antonio Wilson <antonio@ncstore.biz>
2022-10-29 23:58:37 -0400

    Start setting up new forms and edit forms

--- inventory_app/views.py
+++ inventory_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, HttpResponse, redirect
 from user_app.models import User
+import vendor_app
+from .models import Category
+from vendor_app.models import Vendor
 
 # Create your views here.
 def all_products(request):
@@ -25,7 +28,9 @@ def new_product_form(request):
         return redirect("/")
         
     context = {
-        "this_user": User.objects.filter(id=request.session["user_id"])[0]
+        "this_user": User.objects.filter(id=request.session["user_id"])[0],
+        "all_categories": Category.objects.all(),
+        "all_vendors": Vendor.objects.all()
     }
     return render(request, context=context, template_name="new_product.html")
 
--- vendor_app/models.py
+++ vendor_app/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from user_app.models import User
 
 # Create your models here.
 class Address(models.Model):
     address = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=5)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -14,3 +16,4 @@ class Vendor(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     address = models.ForeignKey(Address, related_name="addresses", on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, related_name="users", on_delete=models.CASCADE)
